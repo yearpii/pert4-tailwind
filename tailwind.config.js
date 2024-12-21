03c74a3e37c3fa4fc2e847c9a12f0e3a1edabf64
Satrioagung10 <razorcallahan54@gmail.com>
2024-12-21 09:11:28 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#F54748"
+      },
+      width: {
+        "desktop": "1200px",
+        "hp":"90%"
+      },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+    },
+  },
+  plugins: [],
+}
